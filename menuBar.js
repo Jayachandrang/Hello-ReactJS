8d7d2b58f43bde9e795cd6f948e217f4a77e52a0
Jayachandran <gchandranmca@gmail.com>
2017-07-13 18:50:22 +0530

    Create menuBar.js
    menu Bar component with getInitial State. State updating with index of focused menu

---
+++ menuBar.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import './menu.css';
+
+
+var menuBar = React.createClass({
+
+    getInitialState: function(){
+        return { focused: 0 };
+    },
+
+    clicked: function(index){
+
+        // The click handler will update the state with
+        // the index of the focused menu entry
+
+        this.setState({focused: index});
+    },
+
+    render: function() {
+        // Here we will read the items property, which was passed
+        // as an attribute when the component was created
+
+        var self = this;
+
+        // The map method will loop over the array of menu entries,
+        // and will return a new array with <li> elements.
+
+        return (
+            <div>
+                <ul>{ this.props.items.map(function(m, index){
+        
+                    var style = '';
+        
+                    if(self.state.focused == index){
+                        style = 'focused';
+                    }
+        
+                    // Notice the use of the bind() method. It makes the
+                    // index available to the clicked function:
+        
+                    return <li key={index} className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
+        
+                }) }
+                        
+                </ul>
+                
+                <p>You have been selected: {this.props.items[this.state.focused]}</p>
+            </div>
+        );
+
+    }
+});
+
+export default menuBar;
