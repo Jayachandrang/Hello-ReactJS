3900b7fb4bde24f4906f13fc4e105064855050b8
Jayachandran <gchandranmca@gmail.com>
2017-07-13 17:59:54 +0530

    Update webpack.config.js
    Added Style Loader and Css loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ var config = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   }
